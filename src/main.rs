5c53ebb135a08a6c601d60dd7e1c27f8ad487f3f
Rudolfs Agris Stilve <rudolfsagris@gmail.com>
2020-02-12 09:13:28 +0200

    AAAAAAAAAAAAAAAAAAAAa

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use stringplus::*;
 
 #[derive(Debug)]
 struct Lesson {
@@ -8,12 +9,6 @@ struct Lesson {
     all_text: String,
     time_start: i64,
     time_end: i64
-}
-
-#[derive(Debug)]
-struct Day {
-    day: u32,
-    lessons: Vec<Lesson>
 }
 
 #[tokio::main]
@@ -27,26 +22,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let calendar_day_selector = scraper::selector::Selector::parse("td.calendar-day").unwrap();
     let event_selector = scraper::selector::Selector::parse("div.event").unwrap();
     let day_number_selector = scraper::selector::Selector::parse("div.day-number").unwrap();
-    let mut days: Vec<Day> = vec!();
+    //let mut days: Vec<Day> = vec!();
 
     for day in doc.select(&calendar_day_selector) {
         let mut events: Vec<Lesson> = vec!();
 
         for event in day.select(&event_selector) {
             let all_text = event.inner_html();
-            let time_interval_iter = all_text.chars().take(11);
-            let time_start = time_interval_iter.take(5).collect();
-
-            let new_event = Lesson {all_text: event.inner_html()};
-            events.push(new_event);
+            //let time_interval_iter = all_text.chars().take(11);
+            //let time_start = time_interval_iter.take(5).collect();
+            println!("{}", all_text);
+            //let new_event = Lesson {all_text: event.inner_html()};
+            //events.push(new_event);
+            let time_start = all_text.as_str().substring(0,5);
+            let time_end = all_text.as_str().substring(6,5);
+            println!("Sakums: {} Beigas: {}", time_start, time_end);
         }
 
-        let day_number = day.select(&day_number_selector).last().unwrap()
+        /*let day_number = day.select(&day_number_selector).last().unwrap()
         .inner_html().parse::<u32>().unwrap();
 
         let new_day = Day {day: day_number, lessons: events};
 
-        println!("{:#?}", new_day);
+        println!("{:#?}", new_day);*/
     }
 
     Ok(())
